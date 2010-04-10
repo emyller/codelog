798a96f6aab67ca1f0c0af20ee14fc052565d98b
Evandro Myller <evandromyller@gmail.com>
2010-04-10 17:02:15 -0300

    production settings. ready to go! :D

---
+++ psettings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'web',
+    }
+}
