e50a389c4977ba1d7e55f740f97b421aa262d67b
Evandro Myller <evandromyller@gmail.com>
2010-04-10 04:18:23 -0300

    tuples doesn't need () at all :)

--- core/admin.py
+++ core/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 import models
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'datetime',)
+    list_display = 'title', 'datetime',
 
 admin.site.register(models.Post, PostAdmin)
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = 'name',
 
 admin.site.register(models.Tag, TagAdmin)
