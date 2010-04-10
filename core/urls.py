32975a1fdde420ab5f28d770642eaeca9b6b5dc3
Evandro Myller <evandromyller@gmail.com>
2010-04-10 13:49:56 -0300

    filtering posts by tag

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import redirect_to
 import views
 
 urlpatterns = patterns('core.views',
-    url(r'^$', redirect_to, { 'url': 'latest/'}, name='home'),
-    url(r'^latest/$', 'latest'),
-    url(r'^post/(?P<post_id>\d+)$', 'view_post', name='post'),
+    url(r'^$', redirect_to, { 'url': 'latest/'}),
+    url(r'^latest/$', 'latest', name='home'),
+    url(r'^post/(\d+)$', 'view_post', name='post'),
 )
--- core/views.py
+++ core/views.py
@@ -3,8 +3,10 @@ from django.views.generic import list_detail
 from models import *
 
 def latest(req):
+    posts = Post.objects.all()
+    if req.GET.get('tag'): posts = posts.filter(tags__name=req.GET['tag'])
     return list_detail.object_list(req,
-        queryset = Post.objects.all().select_related(),
+        queryset = posts,
         paginate_by = int(req.GET.get('rpp') or 5),
         template_name = 'core/latest.html',
         template_object_name = 'post',
@@ -12,7 +14,7 @@ def latest(req):
 
 def view_post(req, post_id):
     return list_detail.object_detail(req,
-        queryset = Post.objects.all().select_related(),
+        queryset = Post.objects.all(),
         object_id = post_id,
         template_name = 'core/post.html',
         template_object_name = 'post',
