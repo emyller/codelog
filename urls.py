d24d2711a0799d2c1bd6b4d938f2a04a86a07922
Evandro Myller <evandromyller@gmail.com>
2010-04-10 01:41:20 -0300

    admin enabled in urls

--- urls.py
+++ urls.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^blog/', include('blog.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+
+## static files
+if not settings.IN_PRODUCTION:
+    urlpatterns += patterns('',
+        (r'^{0}/(?P<path>.*)$'.format(settings.MEDIA_URL[1:-1]), 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        })
+    )
