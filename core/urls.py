d9094e79a422a1829f2f02d2e1d6c1f6d59f5b1d
Evandro Myller <evandromyller@gmail.com>
2010-04-10 04:13:26 -0300

    pagination

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
 import views
 
 urlpatterns = patterns('core.views',
-    url(r'^$', 'home'),
+    url(r'^$', redirect_to, { 'url': 'latest/'}),
+    url(r'^latest/$', 'home'),
 )
--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from models import *
 def home(req):
     return list_detail.object_list(req,
         queryset = Post.objects.all(),
+        paginate_by = int(req.GET.get('rpp') or 5),
         template_name = 'core/home.html',
         template_object_name = 'post',
     )
