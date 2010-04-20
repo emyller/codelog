dbab0dc6c492d0cbe0bb63aebea928f94245f89a
Evandro Myller <evandromyller@gmail.com>
2010-04-20 05:29:04 -0300

    js show plain code helper improved

--- _media/js/ux.js
+++ _media/js/ux.js
@@ -6,20 +6,22 @@ var OPTIONS = { 'duration': 300 };
 // insert the toolbox in every code block
 u('.codehilite').prepend('div.tools');
 
-// apply a nice effect to the toolbox
-u('.codehilite').on('mouseenter,mouseleave', function (e) {
-    u(this).first().anim({ 'opacity': +(e.type == 'mouseenter') * 1 }, OPTIONS) });
-
 // copy code blocks to textareas
 for (var i = -1, blocks = u('.codehilite'), block; blocks[++i];) {
     block = u(blocks[i]);
     block.append('textarea', block.text()).hide() }
 
-// adds button to show/hide the created textareas
 u('.codehilite .tools')
-    .append('button', 'toggle plain code').on('click', function () {
-        var block = u(this).up(2);
-        block.first('textarea').css(block.first('pre').size()).toggle().prev().toggle() });
+    // adds button to show/hide the created textareas
+    .append('button', 'select code').on('click', function () {
+        var block = u(this).up(2),
+            textarea = block.first('textarea');
+        textarea.css(block.first('pre').size()).toggle().prev().toggle();
+        textarea[0].select() });
+
+// apply a nice effect to the toolbox
+u('.codehilite').on('mouseenter,mouseleave', function (e) {
+    u(this).first().anim({ 'opacity': +(e.type == 'mouseenter') * 1 }, OPTIONS) });
 
 // opacity effect for buttons
 u('.codehilite .tools').css({ 'opacity': 0 })
